7d0aea90c2fb8566479ffae7d7aaf3e95e7d0e92
Young-il Cho <youngilcho@nhnent.com>
2018-06-26 17:08:01 +0900

    add two examples for switch()

---
+++ switch1.js
@@ -0,0 +1,23 @@
+const Rx = require('@reactivex/rxjs');
+
+const source = Rx.Observable.range(0, 3)
+    //.select(function (x) {
+    .map(function (x) {
+        return Rx.Observable.range(x, 3);
+    })
+    .switch();
+// range(0, 3) -> 0 1 2
+// -> range(0, 3) -> 0 1 2
+// -> range(1, 3) -> 1 2 3
+// -> range(2, 3) -> 2 3 4
+const subscription = source.subscribe(
+    function (x) {
+        console.log("Next:", x);
+    },
+    function (err) {
+        console.log("Error:", err);
+    },
+    function () {
+        console.log("Completed");
+    }
+);
