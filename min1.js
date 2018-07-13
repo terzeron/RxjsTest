6ed88a3c7cb9b3d841cb135beee381cd9cb80d21
Young-il Cho <youngilcho@nhnent.com>
2018-07-13 15:32:45 +0900

    add some examples

---
+++ min1.js
@@ -0,0 +1,16 @@
+const Rx = require('@reactivex/rxjs');
+
+const source = Rx.Observable.from([1, 3, 5, 7, 9, 2, 4, 6, 8, 1])
+    .min();
+
+const subscription = source.subscribe(
+    function (x) {
+        console.log(new Date(), "Next:", x);
+    },
+    function (err) {
+        console.log(new Date(), "Error:", err);
+    },
+    function () {
+        console.log(new Date(), "Completed");
+    }
+);
